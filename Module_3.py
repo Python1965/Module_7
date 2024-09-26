8f29db81a7039015ff6839e659042888222bd832
Python1965 <python65@rambler.ru>
2024-09-26 22:17:42 +0300

    Add files via upload

--- Module_3.py
+++ Module_3.py
@@ -118,10 +118,11 @@ def start():
     print(finder1.find('TEXT'))   # 3 слово по счёту
     print(finder1.count('teXT'))  # 4 слова teXT в тексте всего
 
-    #inder2 = WordsFinder('test_file.txt')
-    # print(finder2.get_all_words())  # Все слова
-    # print(finder2.find('TEXT'))  # 3 слово по счёту
-    # print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+    print()
+    finder2 = WordsFinder('test_file.txt')
+    print(finder2.get_all_words())  # Все слова
+    print(finder2.find('TEXT'))  # 3 слово по счёту
+    print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
 
 
 if __name__ == '__main__':
