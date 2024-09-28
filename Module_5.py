4e0f25ded8cda2a9b31d3a9e97310f12e3dd2862
Python1965 <python65@rambler.ru>
2024-09-28 12:00:08 +0300

    Add files via upload

--- Module_5.py
+++ Module_5.py
@@ -13,35 +13,45 @@
 # for root, dirs, files in os.walk(directory):
 #   for file in files:
 #     filepath = ?
-#     filetime = ?
+#     filetime =
 #     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 #     filesize = ?
 #     parent_dir = ?
 #     print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 # ****************************************************************************************
 
-import os.path
+import os
 import time
 
-def file_info():
 
-    for dirpath, dirnames, filenames in os.walk('.'):
-        print(f'Найден каталог: {dirpath}')
-        print(f'Список поддиректорий {dirnames}')
-        
+def file_info(cur_dir):
+
+    for dirpath, dirnames, filenames in os.walk(cur_dir):
+        print()
+        print(f'Текущий каталог: {os.path.abspath(dirpath)}')
+        print(f'Список подкаталогов: {dirnames}')
+
         for filename in filenames:
-            print(f'Найден файл: {filename}')
-            filepath = ?
-            filetime = ?
+            filepath = os.path.join(dirpath, filename)
+            filetime = os.path.getmtime(filepath)
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-            filesize = ?
-            parent_dir = ?
-            print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+            filesize = os.path.getsize(filepath)
+            parent_dir = dirpath
+            print(f'    Найден файл: {filename}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
+        for dirname in dirnames:
+            new_dir = os.path.join(cur_dir, dirname).replace('.', "", 1)
+            file_info(new_dir)
+
+
+def cat_stru_printing():
+    work_dir = os.getcwd()
+    print(f'Рабочий каталог: {work_dir}')
+    file_info(work_dir)
 
 
 def start():
-    file_info()
+    cat_stru_printing()
 
 if __name__ == '__main__':
     start()
